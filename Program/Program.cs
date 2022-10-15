351a70d3ca7274b2d817a4b48f5a9cb4a3dbfe2e
NIKSAN <NIKSAN2104@mail.ru>
2022-10-16 02:08:19 +0800

    Добавил Program

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,1 +1,24 @@
-﻿// Начало работы!
+﻿string[] array1 = new string[5] {"one", "russia", "the", "4567", "123"};
+string[] array2 = new string[array1.Length];
+void SelectionArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SelectionArray(array1, array2);
+PrintArray(array2);
